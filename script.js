926ef6c269b30d6823c646b939c87c5590e33330
Jakub Murszewski <jakubmurszewski@gmail.com>
2020-05-29 21:55:22 +0200

    add tokenization, keywords, and responses to the chatbot script

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ startButton.onclick = function() {
   console.log('Start speaking!');
 }
 
+// KEYWORDS
+var Temperature = false;
+var Humidity = false;
+let City ="";
+
 var conversation = document.querySelector(".conversation");
 
 recognition.onresult = function(event) {
@@ -24,22 +29,61 @@ recognition.onresult = function(event) {
   conversation.append('You: ' + utt);
   conversation.appendChild(linebreak);
   console.log('Confidence: ' + event.results[0][0].confidence);
-  cityName = utt;
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=a4b92e45ab61cfc3cb9be1af36127c2d";
-    fetch(url)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response)
-        useSpeechSynth("Temperature in " + cityName + " is " + response.main.temp + " Kelvin");
-        conversation.append("Weatherbot: " + "Temperature in " + cityName + " is " + response.main.temp + " Kelvin");
-        var linebreak2 = document.createElement("br");
-        conversation.appendChild(linebreak2);
-    }
-        )
+  var Words = utt.split(" ")
+      console.log (Words)
+      for (prop in Words) {
+        console.log(Words[prop] + " " + (typeof (Words[prop])));
+        if (Words[prop] == "temperature") {
+          Temperature = true 
+        }
+      };
+      for (prop in Words) {
+        if (Words[prop] == "humidity") {
+          Humidity = true
+        }
+      };
+      for (prop in Words) {
+        for (item in Words[prop].split("")) {
+          console.log((Words[prop])[item])
+          if ((Words[prop])[item] == (Words[prop])[item].toUpperCase()) {
+            City = Words[prop]
+          }
+        }
+      };
+      console.log("TEMP " + Temperature);
+      console.log("HUM " + Humidity);
+      console.log("CITY " + City)
+
+      let url = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&appid=a4b92e45ab61cfc3cb9be1af36127c2d";
+      fetch(url)
+      .then(response => response.json())
+      .then(response => {
+          if (Temperature == true) {
+              useSpeechSynth("temperature in " + City + " is " + float2int((response.main.temp - 273.15)) + " degrees Celsius")
+              conversation.append("Weatherbot: " + "Temperature in " + City + " is " + float2int((response.main.temp - 273.15)) + "°C. ");
+              var linebreak2 = document.createElement("br");
+              conversation.appendChild(linebreak2);
+              Temperature = false
+
+          };
+          if (Humidity == true) {
+              useSpeechSynth("humidity in " + City + " is " + response.main.humidity + " percent")
+              conversation.append("Weatherbot: " + "Humidity in " + City + " is " + response.main.humidity + " percent.");
+              var linebreak2 = document.createElement("br");
+              conversation.appendChild(linebreak2);
+              Humidity = false 
+          };
+          City = ""
+      }
+          )
+
 
 
   }
 
+function float2int (value) {
+  return value | 0;
+}
 
 
 recognition.onspeechend = function() {
@@ -50,7 +94,7 @@ var synth = window.speechSynthesis;
 
 function useSpeechSynth(text) {
   var utterThis = new SpeechSynthesisUtterance(text);
-  synth.cancel();
+  //synth.cancel();
   utterThis.lang = 'en-US';
   synth.speak(utterThis);
 }
